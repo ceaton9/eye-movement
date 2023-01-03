e4414d64e29501c0601108d924c237012f7106f9
Elang Indra <120548182+ceaton9@users.noreply.github.com>
2023-01-03 11:23:03 +0700

    Generating eye movement

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef } from "react";
+import "./App.css";
 
 function App() {
+  const squares = useRef<HTMLDivElement>(null);
+  const eyeball = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    document.addEventListener("mousemove", (event) => {
+      const posX = event.clientX - window.innerWidth / 2;
+      const posY = event.clientY - window.innerHeight / 2;
+
+      (squares.current as HTMLDivElement).style.transform =
+        "translate(" + posX * 0.01 + "%, " + posY * 0.01 + "%)";
+      (eyeball.current as HTMLDivElement).style.transform =
+        "translate(" + posX * 0.08 + "%, " + posY * 0.08 + "%)";
+    });
+  }, [squares, eyeball]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="container">
+        <div ref={squares} id="square1">
+          <div ref={eyeball} className="eyeball" id="eyeball"></div>
+        </div>
+      </div>
     </div>
   );
 }
